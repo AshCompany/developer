f12dfcf3a411637840d228f0c634f9994ea97f7a
Ashkan Ansari <ashkan.ansari.aa@gmail.com>
2021-11-20 19:34:17 +0330

    pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter,
@@ -6,15 +5,17 @@ import {
   Route
 } from 'react-router-dom';
 import Pageone from './Pageone';
+import Home from './Home';
+import PageTwo from './PageTwo';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/home' element={<div>home</div>} />
+          <Route path='/home' element={<Home />} />
           <Route path='/page-one' element={<Pageone />} />
-          <Route path='/page-two' element={<div>page 2</div>} />
+          <Route path='/page-two' element={<PageTwo />} />
         </Routes>
       </BrowserRouter>
     </div>
